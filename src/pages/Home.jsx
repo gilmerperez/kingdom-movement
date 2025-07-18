3ddc62bbafec3d9ae753c257391329543eb0099d
gilmerperez <gilmer2002@outlook.com>
2025-07-18 19:21:33 -0400

    Added boilerplate content to Header, Footer, and Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <section className={styles.homeContainer}>Home</section>
+      </main>
+    </>
+  );
+}
+
+export default Home;
