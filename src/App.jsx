e8e833b40ac5c325a39edb5590d836e94356a652
gilmerperez <gilmer2002@outlook.com>
2025-07-18 19:03:53 -0400

    Added Header, Footer, and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
