b58dae6bf4d221ff322dd5edddf2dee7827cfe61
gilmerperez <gilmer2002@outlook.com>
2025-07-18 21:33:03 -0400

    Added Header content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <header>
-        <section className={styles.headerContainer}>Header</section>
+        <section className={styles.headerContainer}>
+          {/* Logo */}
+          <div className={styles.logoContainer}>
+            <span className={styles.kngdm}>
+              <NavLink to="/">KNGDM</NavLink>
+            </span>
+            <span className={styles.mvmnt}>
+              <NavLink to="/">MVMNT</NavLink>
+            </span>
+          </div>
+
+          <div className={styles.navContainer}>
+            {/* Navigation */}
+            <nav className={styles.navLinks}>
+              <NavLink to="/">HOME</NavLink>
+              <NavLink to="/pricing">PRICING</NavLink>
+              <NavLink to="/schedule">SCHEDULE</NavLink>
+              <NavLink to="/nutrition">NUTRITION</NavLink>
+            </nav>
+            {/* Free Class CTA */}
+            <button className={styles.ctaButton}>FREE CLASS</button>
+          </div>
+        </section>
       </header>
     </>
   );
