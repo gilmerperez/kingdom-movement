8f6820c5726bcee3a709f031c456d322a387eea6
gilmerperez <gilmer2002@outlook.com>
2025-09-03 19:33:33 -0400

    Made lots of changes to header: Added a sidebar nav instead of dropdown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,47 @@
 import styles from "./Header.module.css";
+import { createPortal } from "react-dom";
 import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Header() {
+  // * Sticky header logic
+  const [isScrollingUp, setIsScrollingUp] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setIsScrollingUp(currentScrollY < lastScrollY || currentScrollY < 10);
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  // * Custom styles for active page
+  const navLinkClass = ({ isActive }) => (isActive ? styles.activeLink : undefined);
+
+  // * Mobile sidebar toggle
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  // Handle hamburger click with spin animation
+  const handleHamburgerClick = () => {
+    setIsSpinning(true);
+    setMenuOpen(true);
+
+    // Reset spinning state after animation completes
+    setTimeout(() => {
+      setIsSpinning(false);
+    }, 300);
+  };
+
+  // * Phone call handler
+  const handlePhoneClick = () => {
+    window.location.href = "tel:(321) 337-5193";
+  };
+
   // * Theme switch
   const [theme, setTheme] = useState("dark");
 
@@ -38,137 +77,135 @@ function Header() {
     return () => mediaQuery.removeEventListener("change", handleChange);
   }, []);
 
-  // * Mobile menu toggle
-  const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => setMenuOpen(!menuOpen);
-
-  // Make mobile menu & hamburger icon disappear after 480px
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 480 && menuOpen) {
-        setMenuOpen(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [menuOpen]);
-
-  // * Sticky header logic
-  const [isScrollingUp, setIsScrollingUp] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      setIsScrollingUp(currentScrollY < lastScrollY || currentScrollY < 10);
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
-
   return (
     <>
       <header className={`${isScrollingUp ? styles.visible : styles.hidden}`}>
         <section className={styles.headerContainer}>
-          {/* Logo */}
-          <NavLink to="/" className={styles.logoContainer}>
-            <img src={theme === "dark" ? "/logo-white.png" : "/logo-black.png"} alt="Kingdom Movement Logo" />
-          </NavLink>
-
-          {/* Site navigation */}
-          <div className={styles.siteNavigation}>
-            <nav className={styles.navLinks}>
-              <NavLink to="/" className={({ isActive }) => `${styles.navLink} ${isActive ? styles.activeLink : ""}`}>
-                HOME
-              </NavLink>
-              <NavLink
-                to="/nutrition"
-                className={({ isActive }) => `${styles.navLink} ${isActive ? styles.activeLink : ""}`}
-              >
-                NUTRITION
-              </NavLink>
-              <NavLink
-                to="/schedule"
-                className={({ isActive }) => `${styles.navLink} ${isActive ? styles.activeLink : ""}`}
-              >
-                SCHEDULE
-              </NavLink>
-              <NavLink
-                to="/membership"
-                className={({ isActive }) => `${styles.navLink} ${isActive ? styles.activeLink : ""}`}
-              >
-                MEMBERSHIP
-              </NavLink>
-            </nav>
-
-            {/* Seperate site navigation and theme button */}
-            <span className={styles.seperator}>|</span>
-
-            {/* Theme button */}
-            <button className={styles.themeButton} onClick={toggleTheme}>
-              <i className={`fa-solid ${theme === "dark" ? "fa-moon" : "fa-sun"} fa-sm`}></i>
-              <p>{theme === "dark" ? "DARK" : "LIGHT"}</p>
-            </button>
-
-            {/* Free class button */}
-            <button className={styles.freeClassButton}>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://rangerschamps.wodify.com/OnlineSalesPage/Main?q=Classes%7COnlineMembershipId%3D236922%26ProgramId%3D0%26LocationId%3D10458%26IsToViewScheduleOnly%3DTrue"
-              >
-                FREE CLASS
-              </a>
-            </button>
+          {/* Desktop header */}
+          <div className={styles.desktopHeader}>
+            {/* Logo */}
+            <NavLink to="/" className={styles.logoContainer}>
+              <img src={theme === "dark" ? "/logo-white.png" : "/logo-black.png"} alt="Kingdom Movement Logo" />
+            </NavLink>
+            <div className={styles.navContainer}>
+              {/* Site navigation */}
+              <nav className={styles.navItems}>
+                <NavLink to="/" className={navLinkClass}>
+                  HOME
+                </NavLink>
+                <NavLink to="/nutrition" className={navLinkClass}>
+                  NUTRITION
+                </NavLink>
+                <NavLink to="/schedule" className={navLinkClass}>
+                  SCHEDULE
+                </NavLink>
+                <NavLink to="/membership" className={navLinkClass}>
+                  MEMBERSHIP
+                </NavLink>
+              </nav>
+              {/* Seperator */}
+              <span className={styles.seperator}>|</span>
+              {/* Theme button */}
+              <button className={styles.themeButton} onClick={toggleTheme}>
+                <i className={`fa-solid ${theme === "dark" ? "fa-moon" : "fa-sun"}`}></i>
+                <p>{theme === "dark" ? "DARK" : "LIGHT"}</p>
+              </button>
+              {/* Free class button */}
+              <button className={styles.freeClassButton}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://rangerschamps.wodify.com/OnlineSalesPage/Main?q=Classes%7COnlineMembershipId%3D236922%26ProgramId%3D0%26LocationId%3D10458%26IsToViewScheduleOnly%3DTrue"
+                >
+                  FREE CLASS
+                </a>
+              </button>
+            </div>
           </div>
 
-          {/* Hamburger menu */}
-          <button
-            className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`}
-            onClick={toggleMenu}
-            aria-label="Toggle mobile menu"
-          >
-            <i className="fa-solid fa-bars fa-xl"></i>
-          </button>
+          {/* Mobile header */}
+          <div className={styles.mobileHeader}>
+            {/* Phone icon */}
+            <button className={styles.phoneButton} onClick={handlePhoneClick}>
+              <i className="fa-solid fa-phone fa-xl"></i>
+            </button>
+            {/* Logo */}
+            <NavLink to="/" className={styles.mobileLogoContainer}>
+              <img src={theme === "dark" ? "/logo-white.png" : "/logo-black.png"} alt="Kingdom Movement Logo" />
+            </NavLink>
+            {/* Hamburger icon */}
+            <button className={styles.hamburger} onClick={handleHamburgerClick}>
+              <i className={`fa-solid fa-bars fa-xl ${isSpinning ? styles.spin : ""}`}></i>
+            </button>
+          </div>
         </section>
-
-        {/* Dropdown menu */}
-        <div className={`${styles.mobileMenu} ${menuOpen ? styles.show : ""}`}>
-          {/* Dropdown theme button */}
-          <button className={`${styles.themeButton} ${styles.dropdownThemeButton}`} onClick={toggleTheme}>
-            <i className={`fa-solid ${theme === "dark" ? "fa-moon" : "fa-sun"} fa-sm`}></i>
-            <p>{theme === "dark" ? "DARK" : "LIGHT"}</p>
-          </button>
-
-          {/* Dropdown site navigation */}
-          <NavLink to="/" onClick={toggleMenu}>
-            HOME
-          </NavLink>
-          <NavLink to="/schedule" onClick={toggleMenu}>
-            SCHEDULE
-          </NavLink>
-          <NavLink to="/nutrition" onClick={toggleMenu}>
-            NUTRITION
-          </NavLink>
-          <NavLink to="/membership" onClick={toggleMenu}>
-            MEMBERSHIP
-          </NavLink>
-
-          {/* Dropdown free class button */}
-          <button className={styles.dropdownFreeClassButton}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://rangerschamps.wodify.com/OnlineSalesPage/Main?q=Classes%7COnlineMembershipId%3D236922%26ProgramId%3D0%26LocationId%3D10458%26IsToViewScheduleOnly%3DTrue"
-            >
-              FREE CLASS
-            </a>
-          </button>
-        </div>
       </header>
+
+      {/* Sidebar */}
+      {menuOpen &&
+        createPortal(
+          <div className={styles.sidebarOverlay} onClick={() => setMenuOpen(false)}>
+            <div className={styles.sidebar} onClick={(e) => e.stopPropagation()}>
+              {/* Sidebar close button */}
+              <button className={styles.sidebarClose} onClick={() => setMenuOpen(false)}>
+                <i className="fa-solid fa-xmark"></i>
+              </button>
+              {/* Sidebar site navigation */}
+              <nav className={styles.sidebarNavItems}>
+                <NavLink to="/" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-house"></i>
+                  HOME
+                </NavLink>
+                <NavLink to="/nutrition" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-apple-alt"></i>
+                  NUTRITION
+                </NavLink>
+                <NavLink to="/schedule" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-calendar"></i>
+                  SCHEDULE
+                </NavLink>
+                <NavLink to="/membership" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-id-card"></i>
+                  MEMBERSHIP
+                </NavLink>
+                {/* Seperator */}
+                <hr className={styles.sidebarSeperator} />
+                {/* Free class button */}
+                <button className={styles.sidebarFreeClassButton}>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://rangerschamps.wodify.com/OnlineSalesPage/Main?q=Classes%7COnlineMembershipId%3D236922%26ProgramId%3D0%26LocationId%3D10458%26IsToViewScheduleOnly%3DTrue"
+                  >
+                    FREE CLASS
+                  </a>
+                </button>
+              </nav>
+              {/* Sidebar footer */}
+              <footer className={styles.sidebarFooter}>
+                {/* Theme button */}
+                <button className={`${styles.themeButton} ${styles.sidebarThemeButton}`} onClick={toggleTheme}>
+                  <i className={`fa-solid ${theme === "dark" ? "fa-moon" : "fa-sun"}`}></i>
+                  <p>{theme === "dark" ? "DARK" : "LIGHT"}</p>
+                </button>
+                {/* Legal pages */}
+                <NavLink to="/contact" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-paper-plane"></i>
+                  CONTACT
+                </NavLink>
+                <NavLink to="/privacy-policy" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-shield-halved"></i>
+                  PRIVACY POLICY
+                </NavLink>
+                <NavLink to="/terms-of-service" className={navLinkClass} onClick={() => setMenuOpen(false)}>
+                  <i className="fa-solid fa-asterisk"></i>
+                  TERMS OF SERVICE
+                </NavLink>
+              </footer>
+            </div>
+          </div>,
+          document.body
+        )}
     </>
   );
 }
